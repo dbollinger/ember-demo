7acdfa17002986b5d0bc51aba71139007b0db2f6
Howie <dbollinger@users.noreply.github.com>
2020-11-08 07:45:23 -0600

    Adds byte notation helper (#17)
    - Adds template helper for byte-notation (closes #7 )

---
+++ app/helpers/byte-notation.js
@@ -0,0 +1,15 @@
+import { helper } from '@ember/component/helper';
+
+export function humanizeBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 b';
+
+  const scale = 1024;
+  const sizes = ['b', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const factor = Math.floor(Math.log(bytes) / Math.log(scale));
+
+  return parseFloat((bytes / Math.pow(scale, factor)).toFixed(decimals)) + ' ' + sizes[factor];
+}
+
+export default helper(function byteNotation([bytes, decimals]) {
+  return humanizeBytes(bytes, decimals);
+});
