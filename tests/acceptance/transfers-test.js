a44a9ac6448ec4cb4a198a79ecdd98579541d244
Howie <howie@redshelf.com>
2020-11-07 20:07:20 -0600

    Updates

--- tests/acceptance/transfers-test.js
+++ tests/acceptance/transfers-test.js
@@ -3,14 +3,16 @@ import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
-module('Acceptance | application', function (hooks) {
+module('Acceptance | transfers', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function (assert) {
-    await visit('/');
+  test('visiting /transfers', async function (assert) {
+    await visit('/transfers');
 
-    assert.equal(currentURL(), '/');
-    assert.dom('h1').hasText('The Application');
+    assert.equal(currentURL(), '/transfers');
+
+    assert.dom('h2').hasText('Transfers Report');
+    assert.dom('table[aria-labelledby="transfers-heading"]').exists('');
 
     await a11yAudit();
     assert.ok(true, 'no a11y errors found!');
