cdd68831f5d451feec50cec48f56de4f5666a8b2
Howie <howie@redshelf.com>
2020-11-08 21:01:51 -0600

    Adds API_KEY (closes #10)

--- app/adapters/transfer-request.js
+++ app/adapters/transfer-request.js
@@ -2,6 +2,11 @@ import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class TransferRequestAdapter extends RESTAdapter {
   namespace = '/api';
+
+  headers = {
+    API_KEY: 'globus'
+  };
+
   pathForType() {
     return 'transfer-requests';
   }
